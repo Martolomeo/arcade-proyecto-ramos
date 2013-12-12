1e332311477fab1d7b2f77a47137752f99959f2d
Martolomeo <mdcalvo@uc.cl>
2013-12-12 18:34:00 -0300

    Agregando terreno
    Ya están inicializadas las clases de los terrenos, falta que detecte
    colisiones con ellos y se detenga ahi.

--- Mapa Betas/Beta 1/Main/Main/Clases.py
+++ Mapa Betas/Beta 1/Main/Main/Clases.py
@@ -1,11 +1,69 @@
 import pygame
 from pygame.locals import*
 
-class PlataformaBaja(self,x,y):
-    pygame.sprite.Sprite.__init__(self)
-    self.image = pygame.image.load("Pbajo.png")
-    self.rect = self.image.get_rect()
-    self.rect.centerx = x
-    self.rect.centery = y
-    self.width = self.image.get_width()
-    #self.hi
+class PlataformaBaja(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("Pbajo.png")
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.centery = y
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+
+class PlataformaAlta(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("Palto.png")
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.centery = y
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+
+#Asumo que querias hacer esto?
+
+class Novatin(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("iwbtg.png")
+        self.image1 = pygame.image.load("iwbtg.png")
+        self.image2 = pygame.transform.flip(self.image1, True, False)
+        self.rect = self.image.get_rect()
+        self.height = self.image.get_height()
+        self.rect.centerx = x
+        self.rect.centery = y-(self.height/2)
+        self.jumpspeed = 20
+        self.speedcero = 0
+        self.fall = 2
+        self.doublejump = True
+    
+    def move (self,n,down):
+        '''n es una variable binaria que indica si el personaje
+        se mueve a la derecha o a la izquierda, 0 para la
+        derecha, 1 para la izquierda'''
+        if n==0:
+            self.rect.centerx += down*15
+            self.image = self.image1
+        elif n==1:
+           self.rect.centerx -= down*15
+           self.image = self.image2
+
+    def jump (self,y,jump):
+        if jump == False:
+            if self.rect.bottom >= y:
+                 self.rect.centery = y-(self.height/2)
+                 self.jumpspeed = 20
+                 self.speedcero = 0
+                 self.doublejump = True
+            else:
+                self.rect.centery -= self.speedcero
+                self.speedcero -= self.fall
+        elif jump == True:
+            self.rect.centery -= self.jumpspeed
+            self.jumpspeed -= self.fall
+            if self.rect.bottom > y:
+                self.rect.centery = y-(self.height/2)
+                self.jumpspeed = 20
+                self.speedcero = 0
+                self.doublejump = True
--- Mapa Betas/Beta 1/Main/Main/Main.py
+++ Mapa Betas/Beta 1/Main/Main/Main.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, sys, Clases
 from pygame.locals import*
 pygame.init()
 
@@ -8,6 +8,10 @@ y = 720
 size = (x,y)
 black = (0,0,0)
 clock = pygame.time.Clock()
+construir = 1
+#Es una variable binaria para saber si hay que construir una etapa o no
+plataformas = []
+#Aca se almacenan los objetos plataformas
 
 #Seteamos la pantalla
 screen = pygame.display.set_mode(size, FULLSCREEN)
@@ -20,4 +24,14 @@ while 1:
         if event.key==K_ESCAPE:
             pygame.quit()
             sys.exit(0)
-    screen.fill(black)
+    if construir == 1:
+        screen.fill(black)
+        plataformas.append(Clases.PlataformaAlta(0, 360))
+        plataformas.append(Clases.PlataformaAlta(1024, 360))
+        plataformas.append(Clases.PlataformaBaja(256, 540))
+        plataformas.append(Clases.PlataformaBaja(768, 540))
+        for i in range(len(plataformas)):
+            screen.blit(plataformas[i].image, (plataformas[i].rect.centerx, plataformas[i].rect.centery))
+        construir = 0
+    pygame.display.flip()
+    

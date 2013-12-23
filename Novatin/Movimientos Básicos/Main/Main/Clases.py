5d280e8a1a9059590ffc37b71b2f77fa44a61832
Eduardo Molina <edumolinal@gmail.com>
2013-12-23 16:56:54 -0300

    No se porque tengo que hacer este commit

--- Novatin/Movimientos Básicos/Main/Main/Clases.py
+++ Novatin/Movimientos Básicos/Main/Main/Clases.py
@@ -15,6 +15,7 @@ class Novatin(pygame.sprite.Sprite):
         self.speedcero = 0
         self.fall = 2
         self.doublejump = True
+        self.bullets = []
     
     def move (self,n,down):
         '''n es una variable binaria que indica si el personaje
@@ -45,6 +46,29 @@ class Novatin(pygame.sprite.Sprite):
                 self.jumpspeed = 20
                 self.speedcero = 0
                 self.doublejump = True
+
+    def shoot (self,shoot,n):
+        
+        if shoot == True:
+            self.bullets.append(Bullet(self.rect.centerx, self.rect.centery, n))
+        for bullet in self.bullets:
+            bullet.move() 
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self,x,y,n):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("bullet.png")
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.centery = y
+        if n==0:
+            self.speed = 20
+        elif n==1:
+            self.speed = -20
+
+    def move(self):
+        self.rect.centerx += self.speed
+        
 
 #    def doublejump(self, y, jump):
 
